30aa4b9fbc40c25321c15274ef31fee3d214d68f
Praveen Nayak <pnayak@hamiltongroupuk.com>
2022-03-22 15:55:14 +0000

    Add project files.

---
+++ CountryAPI/Models/CountryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace CountryAPI.Models
+{
+    public class CountryItem
+    {
+        public long Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsDone { get; set; }
+    }
+}
