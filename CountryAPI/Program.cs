7c06e4fe8ec1e4bef516346bc4ed693859e96ca9
Praveen Nayak <pnayak@hamiltongroupuk.com>
2022-03-22 15:58:43 +0000

    Basic API

--- CountryAPI/Program.cs
+++ CountryAPI/Program.cs
@@ -6,13 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddDbContext<CountryContext>(opt =>
     opt.UseInMemoryDatabase("CountryList"));
 
-//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
